d4261b2eddc667314da036fe19e4fec6937f9e6e
Eric Nelson <eric@sandalsoft.com>
2013-11-18 13:23:52 -0600

    - Added starting.sql.db with DROP, CREATE and INSERT seed values for db - changed all tablenames to lowercase
    - Added 577 files to sqlite and postgres flavors
    - added some cmdline tools in app_notes

--- starting.db.sql
+++ starting.db.sql
@@ -1,23 +1,61 @@
--- SQLITE
--- postgres input: /path/to/psql -d database -U username -W < /the/path/to/sqlite-dumpfile.sql
 
-CREATE TABLE teams (
-  id INTEGER PRIMARY KEY,
-  api_name TEXT,
-  name TEXT,
-  short_name TEXT
-);
+
+DROP TABLE IF EXISTS "statuses";
 CREATE TABLE statuses (
   id INTEGER PRIMARY KEY,
   status TEXT
 );
+INSERT INTO "statuses" VALUES(1,'i');
+INSERT INTO "statuses" VALUES(2,'a');
+INSERT INTO "statuses" VALUES(3,'d');
+INSERT INTO "statuses" VALUES(4,'n');
+INSERT INTO "statuses" VALUES(5,'s');
+INSERT INTO "statuses" VALUES(6,'u');
 
+
+
+
+DROP TABLE IF EXISTS "positions";
 CREATE TABLE positions (
   id INTEGER PRIMARY KEY,
   name TEXT
 );
+INSERT INTO "positions" VALUES(1,'Goalkeeper');
+INSERT INTO "positions" VALUES(2,'Defender');
+INSERT INTO "positions" VALUES(3,'Midfielder');
+INSERT INTO "positions" VALUES(4,'Forward');
 
 
+
+
+DROP TABLE IF EXISTS "teams";
+CREATE TABLE teams (
+  id INTEGER PRIMARY KEY,
+  api_name TEXT,
+  name TEXT,
+  short_name TEXT);
+INSERT INTO "teams" VALUES(1,'Arsenal','Arsenal','ARS');
+INSERT INTO "teams" VALUES(3,'Cardiff City','Cardiff City','CAR');
+INSERT INTO "teams" VALUES(4,'Chelsea','Chelsea','CHE');
+INSERT INTO "teams" VALUES(5,'Crystal Palace','Crystal Palace','CRY');
+INSERT INTO "teams" VALUES(6,'Everton','Everton','EVE');
+INSERT INTO "teams" VALUES(7,'Fulham','Fulham','FUL');
+INSERT INTO "teams" VALUES(8,'Hull City','Hull City','HUL');
+INSERT INTO "teams" VALUES(9,'Liverpool','Liverpool','LIV');
+INSERT INTO "teams" VALUES(10,'Man City','Manchester City','MC');
+INSERT INTO "teams" VALUES(11,'Man Utd','Manchester United','MUN');
+INSERT INTO "teams" VALUES(12,'Newcastle','Newcastle','NEW');
+INSERT INTO "teams" VALUES(13,'Norwich','Norwich City','NOR');
+INSERT INTO "teams" VALUES(14,'Southampton','Southampton','SOU');
+INSERT INTO "teams" VALUES(15,'Stoke City','Stoke City','STK');
+INSERT INTO "teams" VALUES(16,'Sunderland','Sunderland','SUN');
+INSERT INTO "teams" VALUES(17,'Swansea City','Swansea City','SWA');
+INSERT INTO "teams" VALUES(18,'Tottenham','Tottenham Hotspur','TOT');
+INSERT INTO "teams" VALUES(19,'West Brom','West Bromwich Albion','WBA');
+INSERT INTO "teams" VALUES(20,'West Ham','West Ham United','WHU');
+
+
+DROP TABLE IF EXISTS "players";
 CREATE TABLE players (
   id INTEGER PRIMARY KEY,
   transfers_out INTEGER,
@@ -60,17 +98,9 @@ CREATE TABLE players (
   FOREIGN KEY(position_id) REFERENCES positions(id)
 );
 
-CREATE TABLE news (
-  id INTEGER PRIMARY KEY,
-  news_updated TEXT,
-  news_added TEXT,
-  news TEXT,
-  news_return TEXT,
-  player_id INTEGER,
-  created_at TEXT,
-  FOREIGN KEY(player_id) REFERENCES players(id)
-);
 
+
+DROP TABLE IF EXISTS "eventsexplain";
 CREATE TABLE eventsexplain (
   id INTEGER PRIMARY KEY,
   minutes_played TEXT,
@@ -81,6 +111,8 @@ CREATE TABLE eventsexplain (
   FOREIGN KEY(player_id) REFERENCES players(id)
 );
 
+
+DROP TABLE IF EXISTS "fixtures";
 CREATE TABLE fixtures (
   id INTEGER PRIMARY KEY,
   date_time TEXT,
@@ -94,35 +126,7 @@ CREATE TABLE fixtures (
 );
 
 
-
-CREATE TABLE seasonshistory (
-  id INTEGER PRIMARY KEY,
-  season TEXT,
-  minutes_played INTEGER,
-  goals_scored INTEGER,
-  assists INTEGER,
-  clean_sheets INTEGER,
-  goals_conceded INTEGER,
-  own_goals INTEGER,
-  penalties_saved INTEGER,
-  penalties_missed INTEGER,
-  yellow_cards INTEGER,
-  red_cards INTEGER,
-  saves INTEGER,
-  bonus INTEGER,
-  ea_sports_ppi INTEGER,
-  net_transfers INTEGER,
-  value INTEGER,
-  points INTEGER,
-  player_id INTEGER,
-  created_at TEXT,
-  FOREIGN KEY(player_id) REFERENCES players(id)
-);
-
-
-
-
-
+DROP TABLE IF EXISTS "fixtureshistory";
 CREATE TABLE fixtureshistory (
   id INTEGER PRIMARY KEY,
   player_id INTEGER,
@@ -151,8 +155,41 @@ CREATE TABLE fixtureshistory (
 );
 
 
--- DELETE FROM fixtures;
--- DELETE FROM fixtureshistory;
--- DELETE FROM eventsexplain;
--- DELETE FROM players;
--- DELETE FROM seasonshistory;
+DROP TABLE IF EXISTS "news";
+CREATE TABLE news (
+  id INTEGER PRIMARY KEY,
+  news_updated TEXT,
+  news_added TEXT,
+  news TEXT,
+  news_return TEXT,
+  player_id INTEGER,
+  created_at TEXT,
+  FOREIGN KEY(player_id) REFERENCES players(id)
+);
+
+
+
+DROP TABLE IF EXISTS "seasonshistory";
+CREATE TABLE seasonshistory (
+  id INTEGER PRIMARY KEY,
+  season TEXT,
+  minutes_played INTEGER,
+  goals_scored INTEGER,
+  assists INTEGER,
+  clean_sheets INTEGER,
+  goals_conceded INTEGER,
+  own_goals INTEGER,
+  penalties_saved INTEGER,
+  penalties_missed INTEGER,
+  yellow_cards INTEGER,
+  red_cards INTEGER,
+  saves INTEGER,
+  bonus INTEGER,
+  ea_sports_ppi INTEGER,
+  net_transfers INTEGER,
+  value INTEGER,
+  points INTEGER,
+  player_id INTEGER,
+  created_at TEXT,
+  FOREIGN KEY(player_id) REFERENCES players(id)
+);
