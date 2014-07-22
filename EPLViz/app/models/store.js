878350321045e3ca3c9fbdd4d4d56bdd2ccc7e21
Eric Nelson <eric@sandalsoft.com>
2014-07-21 20:55:33 -0500

    - caching working

--- EPLViz/app/models/store.js
+++ EPLViz/app/models/store.js
@@ -1,17 +1,30 @@
 import Ember from 'ember';
 
+var cache = {};
+
 export default Ember.Object.extend({
 
   find: function(name, id) {
-      var adapter = this.container.lookup('adapter:' + name);
-    window.console.log('find() adapter: ' + JSON.stringify(adapter));
-
-      return adapter.find(name, id);
+    if (cache[name] && cache[name][id]) {
+      return cache[name][id];
+    }
+    var adapter = this.container.lookup('adapter:' + name);
+    return adapter.find(name, id).then(function(record) {
+      cache[name] = cache[name] || {};
+      cache[name][id] = record;
+      return record;
+    });
   },
 
   findAll: function(name) {
-      var adapter = this.container.lookup('adapter:' + name);
-      window.console.log('adapter: ' + JSON.stringify(adapter));
-      return adapter.findAll(name);
+    if (cache[name]) {
+      return cache[name];
+    }
+    var adapter = this.container.lookup('adapter:' + name);
+    return adapter.findAll(name).then(function(record) {
+      cache[name] = cache[name] || {};
+      cache[name] = record;
+      return record;
+    });
   },
 });
